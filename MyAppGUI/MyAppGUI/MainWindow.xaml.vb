b5eb21b47c2a6805de10584241c42766c45bd26e
Jetson13 <100637527+Jetson13@users.noreply.github.com>
2022-03-07 16:42:41 -0400

    Delete MyAppGUI/MyAppGUI directory

--- MyAppGUI/MyAppGUI/MainWindow.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Class MainWindow
-    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
-        Hombres
-    End Sub
-
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-
-    End Sub
-End Class
